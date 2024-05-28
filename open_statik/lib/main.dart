0eb6d41ed332b2646d4f0ea8088ae69913f20aa7
ehargunani6949 <ehargunani6949@student.pps.net>
2024-05-28 11:33:32 -0700

    added stream listener to playback audio, nonfunctional

--- open_statik/lib/main.dart
+++ open_statik/lib/main.dart
@@ -5,13 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:record/record.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-
-
-
 void main() {
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -22,9 +18,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(
-            0, 79, 33, 1.0)),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromRGBO(0, 79, 33, 1.0)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Audio Playback'),
@@ -35,16 +30,13 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
-
   final String title;
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  final audioControl = StreamController();
+  //final audioControl = StreamController();
   late AudioRecorder rec;
   late AudioPlayer player;
   late Stream<Uint8List> audio;
@@ -54,6 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     player = AudioPlayer();
     rec = AudioRecorder();
+
     super.initState();
   }
 
@@ -64,54 +57,51 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  Future<void> startPlayback() async{
-    try{
-      if(await rec.hasPermission() && !playing){
-        await audioControl.addStream(await rec.startStream(const RecordConfig(encoder: AudioEncoder.pcm16bits)));
+  Future<void> startPlayback() async {
+    try {
+      if (await rec.hasPermission() && !playing) {
+        //await audioControl.addStream(await rec.startStream(const RecordConfig(encoder: AudioEncoder.pcm16bits)));
+        audio = await rec
+            .startStream(const RecordConfig(encoder: AudioEncoder.pcm16bits));
         print(audio.isBroadcast);
-
+        final audioSubscription = audio.listen((data) => player.play(BytesSource(data)));
 
         setState(() {
           playing = true;
         });
       }
-    }
-    catch(e){
+    } catch (e) {
       print('error: $e');
     }
   }
 
-  Future<void> stopPlayback() async{
-    try{
+  Future<void> stopPlayback() async {
+    try {
       await rec.stop();
       await player.stop();
       setState(() {
         playing = false;
       });
-    }
-    catch(e){
+    } catch (e) {
       print('error: $e');
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.tertiary,
-
         title: Text(widget.title, style: const TextStyle(color: Colors.white)),
       ),
-      body:  Center(
-
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(onPressed: startPlayback, child: const Text('Play audio')),
-            ElevatedButton(onPressed: stopPlayback, child: const Text('Stop audio')),
+            ElevatedButton(
+                onPressed: startPlayback, child: const Text('Play audio')),
+            ElevatedButton(
+                onPressed: stopPlayback, child: const Text('Stop audio')),
           ],
         ),
       ),
